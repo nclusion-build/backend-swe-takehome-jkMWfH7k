6903305755f6abdcc18b16ae8e20351efbe2869a
Van Nguyen <thegoleffect@gmail.com>
2025-09-02 19:41:36 -0700

    Add tests/unit/models/player_model_test.go

---
+++ tests/unit/models/player_model_test.go
@@ -0,0 +1,15 @@
+package models_test
+
+import (
+    "testing"
+    m "project/src/models"
+)
+
+func TestCreatePlayer(t *testing.T) {
+    model := m.NewPlayerModel()
+    // TODO: implement create and assert
+    _ = model
+}
+
+
+// TODO: Add Go unit tests for Game and Player models [ttt.todo.tests.go.unit.models]
